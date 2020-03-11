5a15ef76b0be7632e118e9f58aac8bdfdbb5ded3
Argal Tempest <argal@STC-PSONB06.STCDOM.COM>
2020-03-11 22:04:15 +0800

    Broken Unit test

--- src/SimpleAPI/Controllers/WeatherForecastController.cs
+++ src/SimpleAPI/Controllers/WeatherForecastController.cs
@@ -40,7 +40,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "Stephen Gagatiga";
+            return "Arvin Bonggal";
         }
     }
 }
