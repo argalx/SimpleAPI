0a29f77e941d1a2f6b3cc6e644f8f0edd4297fe6
Argal Tempest <argal@STC-PSONB06.STCDOM.COM>
2020-03-11 17:21:05 +0800

    Initial Comment

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,23 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+        WeatherForecastController arvin = new WeatherForecastController();
+        [Fact]
+        public void GetReturnedWeatherForecast()
+        {
+            var returnValue = arvin.Get(1);
+            Assert.Equal("Stephen Gagatiga", returnValue.Value);
+        }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
